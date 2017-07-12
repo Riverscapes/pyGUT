e55eb7b3f26c7224700919b35ebac80c6e6b2041
bangen <sara.bangen@gmail.com>
2017-07-11 22:07:52 -0700

    Restructuring layout.  Adding additional form percentile thresholds.  Removing low flow roughness and detrended DEM parameters.

--- config.py
+++ config.py
@@ -1,47 +1,50 @@
 # Geomorphic Unit Tool Configuration File
 
-# Last updated: 1/25/2017
+# Last updated: 7/11/2017
 # Created by: Sara Bangen (sara.bangen@gmail.com)
 
 # ---------------------------------------------------------------------
 # User-defined input parameters that must be set before each model run.  Include file extension in all filenames.
 
-# workspace: 	    Path to where the inputs are stored.  This is same path where output will be written.
-# runFolderName:    GUT run output folder name.  If set to 'Default' sequential folder name will be used (e.g., Run_001).
-# bfw:              Site integrated bankfull width (m)
-# ww:			    Site integrated wetted width (m)
-# memTh:			Membership threshold for crisp output
-# bfCL:			    Bankfull centerline shapefile [Required fields: 'Channel', 'CLID']
-# bfPolyShp:	    Bankfull polygon shapefile.
-# bfXS:			    Bankfull cross sections shapefile [Required field: 'Channel']
-# inDet: 		    Detrended DEM raster
-# inDEM:            DEM raster
-# thalwegShp:       Thalweg polyline shapefile
-# wCL               Wetted centerline shapefile
-# wPolyShp:	        Wetted polygon shapefile
-# ***Optional inputs***
-# champUnits:       Champ channel units shapefile
-# inWaterD:		    Water depth raster
-# champGrainSize:   Champ grain size *.csv.  Must have columns: 'ChannelUnitID', 'ChannelUnitNumber', 'D84'.  Filename must end with '*GrainSizeDistributionResults.csv'.
+# Project Level Parameters:
+    # workspace: 	    Path to where the inputs are stored.  This is same path where output will be written.
+    # runFolderName:    GUT run output folder name.  If set to 'Default' sequential folder name will be used (e.g., Run_001).
+
+# Tier 1 Parameters:
+    # bfPolyShp:	    Bankfull polygon shapefile.
+    # bfCL:			    Bankfull centerline shapefile [Required fields: 'Channel', 'CLID']
+    # wPolyShp:	        Wetted polygon shapefile
+    # inDEM:            DEM raster
+
+# Tier 2 Additional Parameters
+    # bowlPercentile:   Residual topography percentile above which a cell will be classfied as a bowl
+    # planePercentile:  Residual topography percentil below which a cell will be classified as a plane
+    # thalwegShp:       Thalweg polyline shapefile
+    # wCL               Wetted centerline shapefile
+
+# Tier 3 Additional Parameters
+    # bfXS:			    Bankfull cross sections shapefile [Required field: 'Channel']
+
 # ---------------------------------------------------------------------
 
-workspace      = r'C:\et_al\Shared\Projects\USA\CHaMP\ResearchProjects\TopographicGUs\wrk_Data\01_TestSites\Logan\LRB_BC3220\2015\VISIT_3413\ModelRuns'
-runFolderName  = 'GUT_2.0\\Run_002'
-bowlPercentile = 50
-bfCL           = 'Inputs/BankfullCL.shp'
+#  Project Level Parameters
+workspace      = r'C:\et_al\Shared\Projects\USA\CHaMP\ResearchProjects\TopographicGUs\wrk_Data\01_TestSites\Lemhi\CBW05583-028079\2012\VISIT_1029\ModelRuns'
+runFolderName  = 'GUT_2.0\\Run_008'
+
+#  Tier 1 Parameters
 bfPolyShp      = 'Inputs/Bankfull.shp'
-bfXS           = 'Inputs/BankfullXS.shp'
+bfCL           = 'Inputs/BankfullCL.shp'
+wPolyShp       = 'Inputs/WaterExtent.shp'
 inDEM          = 'Inputs/DEM.tif'
-inDet          = 'Inputs/Detrended.tif'
+
+#  Tier 2 Additional Parameters
+bowlPercentile = 50  # Default: 50
+planePercentile = 25 # Default: 25
 thalwegShp     = 'Inputs/Thalweg.shp'
 wCL            = 'Inputs/CenterLine.shp'
-wPolyShp       = 'Inputs/WaterExtent.shp'
 
-# Optional: Tier 3 Low Flow Roughness
-lowFlowRoughness = 'FALSE'
-champUnits   = 'Inputs/xxx.shp'
-inWaterD	 = 'Inputs/xxx.img'
-champGrainSize = 'Inputs/xxx.csv'
+#  Tier 3 Additional Parameters
+bfXS           = 'Inputs/BankfullXS.shp'
 
 execfile('tier1.py')
 execfile('tier2.py')
