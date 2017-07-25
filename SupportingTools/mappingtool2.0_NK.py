8bc2767ba7b4999c4cdc263bda2ac3e41b59dbfa
Natalie <n.kramer.anderson@gmail.com>
2017-07-25 12:35:00 -0600

    updating contour layer file and mappingtool

--- SupportingTools/mappingtool2.0_NK.py
+++ SupportingTools/mappingtool2.0_NK.py
@@ -14,17 +14,12 @@ workspace=sys.argv[1]
 #workspace=arcpy.GetParameterAsText(0)
 #workspace=r'E:/GUT/_Asotin/ASW00001-NF-F2P2/VISIT_3949'
 
-Lyrdir=sys.argv[3]
-
-
-#Lyrdir=GetParameterAsText(1)
-#if sys.argv[3]==""
-#Lyrdir=r"E:/GUT/pyGUT/GUTSymbology/LYR"
-
-
-
 runfolder=sys.argv[2]
 #runfolder=r'E:/GUT/_Asotin/ASW00001-NF-F2P2/VISIT_3949/Output/GUT_2.0/Run_005'
+
+Lyrdir=sys.argv[3]
+# Change this to hard code it to your symbology directory if you don't want to change it every time
+Lyrdir=r"E:/GUT/pyGUT/GUTSymbology/LYR"
 
 env.workspace=workspace
 
@@ -32,8 +27,9 @@ print "making list of input layer filepaths..."
 layerfiles=os.listdir(Lyrdir)
 
 
-layers=['ev_Hillshade.lyr','out_FlowUnit.lyr',
-        'out_Tier2_UnitForm.lyr', 'ev_Contour.lyr']
+layers=['ev_Hillshade.lyr',
+        'out_Tier2_UnitForm.lyr', 'out_FlowUnit.lyr',
+        'ev_Contour.lyr', 'in_Thalweg.lyr']
 
 #Tier3=Lyrdir+ "/" +'out_Tier3_InChannel.lyr'   
 Tier2=Lyrdir+ "/" +'out_Tier2_UnitForm.lyr'
@@ -48,7 +44,7 @@ print "done"
 print "making list of input shapefile paths..."
 dTier2=runfolder+"/Tier2_InChannel.shp"
 dTier1=runfolder + "/Tier1.shp"
-dcontour=workspace+"/EvidenceLayers/DEM_contours.shp"
+dcontour=workspace+"/EvidenceLayers/DEM_Contours.shp"
 dhillshade=workspace+"/EvidenceLayers/hillshade.tif"
 dthalweg=workspace+"/Inputs/Thalweg.shp"
 
@@ -65,22 +61,21 @@ print "done"
 df = arcpy.mapping.ListDataFrames(mapdoc)[0]
 
 
+#def hillshadefromDEM(inRaster):
+ #   if os.path.exists(datalist[0])== False:
+#        outHillshade = workspace + "/EvidenceLayers/hillshade.tif"
+#        arcpy.HillShade_3d(inRaster, outHillshade)
+#        updateLyr = arcpy.mapping.Layer(datalist[0])
+#        updateLyr = arcpy.mapping.Layer(UpdateLyr)
+        #if os.path.exists(inputlist[0]):
+            #sourceLayer = arcpy.mapping.Layer(inputlist[0])
+            #arcpy.mapping.UpdateLayer(df, updateLyr, sourceLayer)
+        #arcpy.mapping.AddLayer(df,updateLyr)
+#        del updateLyr, sourceLayer
 
-def hillshadefromDEM(inRaster):
-    if os.path.exists(datalist[0])== False:
-        outHillshade = workspace + "/EvidenceLayers/hillshade.tif"
-        arcpy.HillShade_3d(inRaster, outHillshade)
-        updateLyr = arcpy.mapping.Layer(datalist[0])
-        updateLyr = arcpy.mapping.Layer(UpdateLyr)
-        if os.path.exists(inputlist[0]):
-            sourceLayer = arcpy.mapping.Layer(inputlist[0])
-            arcpy.mapping.UpdateLayer(df, updateLyr, sourceLayer)
-        arcpy.mapping.AddLayer(df,updateLyr)
-        del updateLyr, sourceLayer
-
-if os.path.exists(workspace + "/Inputs/DEM.tif")==True:
-    hillshadefromDEM(workspace+"/Inputs/DEM.tif")
-    print "hillshade finished"
+#if os.path.exists(workspace + "/Inputs/DEM.tif")==True:
+#    hillshadefromDEM(workspace+"/Inputs/DEM.tif")
+#    print "hillshade finished"
 
 print "adding layers to map with appropriate symbology"
 for i in range(len(inputlist)):
