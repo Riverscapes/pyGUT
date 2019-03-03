d53f854206b1a92ac6ebbeaf8303ebeda6e2f0b1
WHTNLAB-6MCHHK2\SaraBangen <sara.bangen@gmail.com>
2019-03-03 12:14:18 -0800

    Re-working script and adding ROxygen comments

--- SupportingTools/RScripts/Development/create_fish_pts.R
+++ SupportingTools/RScripts/Development/create_fish_pts.R
@@ -1,186 +1,144 @@
-#Creates shape files of predicted fish points from HSI and NREI as well as NREI points.
-#Also creates a raster of NREI values within the suitable habitat regions.
-
-#Natalie Kramer (n.kramer.anderson@gmail.com)
-#updated Oct 28, 2017
-
-####inputs#####
-#fisrunpath: a path to the NREI visit folder.  To run HSI must be in file structure as is.  
-#Script will break if file structure changes.
-#example:
-#fishrunpath="E:\\Box Sync\\ET_AL\\Projects\\USA\\ISEMP\\GeomorphicUnits\\FifteenStartingSites\\Data\\VisitData/VISIT_1029/NREI"
-
-#GUTrunpath: a path to the GUT run that the analysist is interested in.
-#example: 
-#GUTrunpath="E:\\Box Sync\\ET_AL\\Projects\\USA\\ISEMP\\GeomorphicUnits\\FifteenStartingSites\\Data\\VisitData/VISIT_1029/GUT/Output/GUT_2.0/EqTransMT_01"
-
-
-####outputs#####
-#predFishLocations.shp: ESRI shapefile of predicted fish points from predFishLocations.csv
-#predReddLocs.shp: ESRI Shapefile of predicted redd locations from predReddLocs.csv
-#predReddLocs.shp: ESRI Shapefile of predicted redd locations from predReddLocs.csv
-#allNreiPts.csv: ESRI Shapefile of all NREI pts from allNreiPts.csv
-#SuitableNreiPts.shp: ESRI Shapefile of NREI pts >0 and above 40%pval threshold. 
-#These are the pool of points that the predicted fish locs were chosen from.
-#Only one point per x,y is chosen based on optional argument zrank
-#zrank="max" which takes the maximum NREI from the water column at each location
-#zrank="XX" takes a number specifying the water column placement. 1= bed of stream.
-#suitableNreiRaster.tiff: GeoTiff created from SuitableNreiPts.shp. 
-#This raste is used to create the habitat polygons.
+# Creates point shapefiles from csvs of HSI and NREI predicted fish locations
+# Also creates a raster of NREI values within the suitable habitat regions
+#
+# Args:
+#   fisrunpath: a path to the NREI visit folder.  To run HSI must be in file structure as is.
+#               example:
+#               fishrunpath="E:\\Box Sync\\ET_AL\\Projects\\USA\\ISEMP\\GeomorphicUnits\\FifteenStartingSites\\Data\\VisitData/VISIT_1029/NREI"
+#
+# Returns:
+#   predFishLocations.shp: ESRI shapefile of predicted fish points from predFishLocations.csv
+#   predReddLocations.shp: ESRI Shapefile of predicted redd locations from predReddLocs.csv
+#   allNreiPts.shp: ESRI Shapefile of all NREI pts from allNreiPts.csv
+#   SuitableNreiPts.shp: ESRI Shapefile of NREI pts >0 and above 40%pval threshold. 
+#                        These are the pool of points that the predicted fish locs were chosen from
+#                        Only one point per x,y is chosen based on optional argument zrank
+#                        zrank="max" which takes the maximum NREI from the water column at each location
+#                        zrank="XX" takes a number specifying the water column placement. 1= bed of stream.
+#   suitableNreiRaster.tiff: GeoTiff created from SuitableNreiPts.shp. Used to create habitat polygons.
 
 
-library(sp)
-library(tidyverse)
-library(raster)
-library(rgeos)
-library(rgdal)
 
-Createfishpts=function(fishrunpath, zrank="max", plotNREI=F){  
+#' Create points shapefile from csv
+#'
+#' @param visit.dir Full filepath to visit folder 
+#' @param visit.crs CRS (projection) for visit
+#' @param file.name Name of the csv
+#' @param out.name Name of the output shapefile
+#'
+#' @return Creates a points shapefile from the csv and saves output to same directory as csv
+#' @export
+#'
+#' @examples
+#' create.pts("C:/etal/Shared/Projects/USA/GUTUpscale/wrk_Data/VISIT_1027", 
+#' "+proj=utm +zone=12 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0",
+#' "predFishLocations.csv", "predFishLocations")
+create.pts = function(visit.dir, visit.crs, file.name, out.name){
   
-  #extracts just the visit number 
-  visit=extractvisitfrompath(fishrunpath)
+  # get path to csv file
+  pts.csv = unlist(list.files(path = visit.dir, pattern = file.name, full.names = TRUE, recursive = TRUE, include.dirs = FALSE))
   
-  #Creates path to GUT data based on NREI path
-  GUTpath=paste(strsplit(fishrunpath, "NREI"),"GUT",sep="")
-  HSIpath=paste(strsplit(fishrunpath, "NREI"),"HSI//Output",sep="")
-  Reddpath=paste(strsplit(Fishrunlist[i], "NREI"),"HSI//reddPlacement",sep="")
-
+  # read in csv 
+  pts.tb = read_csv(pts.csv)
   
+  # if 'X' or 'Y' are lowercase change to uppercase
+  if('x' %in% colnames(pts.tb)){pts.tb = rename(pts.tb, X = x)}
+  if('y' %in% colnames(pts.tb)){pts.tb = rename(pts.tb, Y = y)}
   
-  #calls in GUT for same visit and extracts coordinate information
-  if(file.exists(paste(GUTpath, "Inputs\\Thalweg.shp",sep="\\"))){
-    form.poly=readOGR(paste(GUTpath, "Inputs\\Thalweg.shp",sep="\\"))
-    if(gIsValid(form.poly)==F){
-      form.poly=gBuffer(form.poly, byid=TRUE, width=0)
-      print("geometry fixed")
-    }
-    proj <- crs(form.poly)
-    print(proj)
-    
-    
-    #making predicted fish locations from NREI
-  #  if(file.exists(paste(fishrunpath, "\\predFishLocations.shp",sep=""))==F){   
-    if(file.exists(paste(fishrunpath, "\\predFishLocations.csv", sep=""))==T){ 
-      #reads in fish locations and creates spatial points data frame
-      fish=read.csv(paste(fishrunpath,"\\predFishLocations.csv", sep=""), stringsAsFactors = FALSE)
-      if(length(fish[,1])>0){
-        if(exists("proj")){
-          fish.pts <- SpatialPointsDataFrame(fish[,2:3],fish, proj4string = proj)
-        } else {print("unable to determine projection for data")}
-        print("writing NREI predFishLocations.shp")
-        writeOGR(fish.pts, fishrunpath, "predFishLocations", driver="ESRI Shapefile", overwrite_layer = TRUE)
-      } else {print("no NREI predicted fish")}
-    } else {print("no NREI predicted fish locations datafile")}
- #   }
-    
-    #"making predicted redd locations from HSI"
-    #if(file.exists(paste(HSIpath, "\\predReddLocs.shp", sep=""))==F){ 
-    #
-    #  if(file.exists(paste(HSIpath, "\\predReddLocs.csv", sep=""))==T){ 
-    #    #reads in fish locations and creates spatial points data frame
-    #    fish=read.csv(paste(HSIpath,"\\predReddLocs.csv", sep=""), stringsAsFactors = FALSE)
-    #    if(length(fish[,1])>0){
-    #      if(exists("proj")){
-    #        fish.pts <- SpatialPointsDataFrame(fish[,2:3],fish, proj4string = proj)
-    #      } else {print("unable to determine projection for data")}
-    #      print("writing HSI predReddLocs.shp")
-    #      writeOGR(fish.pts, HSIpath, "predReddLocs", driver="ESRI Shapefile", overwrite_layer = TRUE)
-    #    } else {print("no HSI predicted redd")}
-    #  } else {print("no HSI predicted redd locations datafile")}
-    #}
-    
-    
-    #for (i in c(1:length(Fishrunlist))){
-
-      #if(file.exists(paste(Reddpath, "\\chkPredReddLocs.shp",sep=""))==F){
-      if(file.exists(paste(Reddpath, "\\chkPredReddLocs.csv", sep=""))==T){ 
-        #reads in fish locations and creates spatial points data frame
-        fish=read.csv(paste(Reddpath,"\\chkPredReddLocs.csv", sep=""), stringsAsFactors = FALSE)
-        if(length(fish[,1])>0){
-          if(exists("proj")){
-            fish.pts <- SpatialPointsDataFrame(fish[,2:3],fish, proj4string = proj)
-          } else {print("unable to determine projection for data")}
-          print("writing chnk HSI predReddLocs.shp")
-          writeOGR(fish.pts, Reddpath, "chkpredReddLocs", driver="ESRI Shapefile", overwrite_layer = TRUE)
-        } else {print("no chnk HSI predicted redd")}
-      } else {print("no HSI chnk predicted redd locations datafile")}
-      #}
-      
-      #if(file.exists(paste(Reddpath, "\\sthdPredReddLocs.shp",sep=""))==F){
-      if(file.exists(paste(Reddpath, "\\sthdPredReddLocs.csv", sep=""))==T){ 
-        #reads in fish locations and creates spatial points data frame
-        fish=read.csv(paste(Reddpath,"\\sthdPredReddLocs.csv", sep=""), stringsAsFactors = FALSE)
-        if(length(fish[,1])>0){
-          if(exists("proj")){
-            fish.pts <- SpatialPointsDataFrame(fish[,2:3],fish, proj4string = proj)
-          } else {print("unable to determine projection for data")}
-          print("writing sthd HSI predReddLocs.shp")
-          writeOGR(fish.pts, Reddpath, "sthdpredReddLocs", driver="ESRI Shapefile", overwrite_layer = TRUE)
-        } else {print("no sthd HSI predicted redd")}
-      } else {print("no HSI sthd predicted redd locations datafile")}
-      #}
-    #}
-    
-    
-    if(plotNREI==T){
-      #makes NREI points
-      #if(file.exists(paste(fishrunpath, "\\allNreiPts.shp", sep=""))==F){ 
-      if(file.exists(paste(fishrunpath, "\\allNreiPts.csv", sep=""))==T){ 
-        
-        data=read.csv(paste(fishrunpath,"\\allNreiPts.csv", sep=""), stringsAsFactors = FALSE)
-        
-        if(exists("proj")){
-          nrei.pts <- SpatialPointsDataFrame(data[,2:3],data, proj4string = proj)
-        } else {print("unable to determine projection for data")}
-        
-        print("writing allNreiPts.shp")
-        writeOGR(nrei.pts, fishrunpath, "allNreiPts", driver="ESRI Shapefile", overwrite_layer = TRUE)
-        
-        data0=data[which(data$rad.step.gte.user.pval>0 & data$nrei_Jph>0),]
-        
-        
-        #making suitable NREI points
-        #Chooses NREI to be max at each water columnn depth when creating raster
-        if(zrank=="max"){
-          data1 <- data0 %>%
-            dplyr::group_by(X, Y) %>%
-            dplyr::mutate(
-              max.nrei = max(nrei_Jph),
-              is.max.nrei = near(max.nrei, nrei_Jph)
-            ) %>%
-            dplyr::filter(is.max.nrei) %>%
-            dplyr::mutate(nrei_js = max.nrei) %>%
-            dplyr::select(idx, X, Y, Z, zrank,rad.step.gte.user.pval, nrei_Jph)
-        }
-        
-        #chooses NREI to be from a specific z rank value
-        if(is.numeric(zrank)==T){
-          data1=data0[which(data0$nrei_Jph>0 & data0$zrank==zrank),]
-        }
-        
-        
-        if(file.exists(paste(fishrunpath, "\\suitableNreiPts.shp", sep=""))==F){ 
-          if(dim(data1)[1]>0){
-            if(exists("proj")){
-              fish.pts2 <- SpatialPointsDataFrame(data1[,2:3],data1, proj4string = proj)
-              print("writing suitableNreiPts.shp")
-              writeOGR(fish.pts2, fishrunpath, "suitableNreiPts", driver="ESRI Shapefile", overwrite_layer = TRUE)
-            } else {print("unable to determine projection for data did not create shapefile")}
-            
-            #making suitable NREI raster
-            if(dim(data1)[1]>1){
-              pixels <- SpatialPixelsDataFrame(fish.pts2, tolerance=.001, fish.pts2@data)
-              data.raster <- raster(pixels[,'nrei_Jph'])
-              print("writing suitableNreiRaster.tif")
-              raster::writeRaster(data.raster, paste(fishrunpath,"//suitableNreiRaster", sep=""), format="GTiff", overwrite = TRUE)
-            }  else {print("One or less good NREI -- unable to make NREI RASTER") }
-          } else {print("no suitable pts")}
-        }
-      }
-    }
-    
-  } else {print("Thalweg.shp does not exist can't define projection")}  
+  # convert to spatial points data frame
+  coords = cbind(pts.tb$X, pts.tb$Y)
+  pts.spdf = SpatialPointsDataFrame(coords, pts.tb, proj4string = CRS(visit.crs))
   
-  
+  # write output to esri shapefile
+  writeOGR(pts.spdf, dirname(pts.csv), out.name, driver="ESRI Shapefile", overwrite_layer = TRUE)
   
 }
+
+
+#' Checks if fish/redd points shapefile exists.  Creates relevant shapefile if csv input data exists.
+#'
+#' @param data Dataframe created from 'check_visit_data.R' script
+#' @param zrank Value user to select single NREI value for each xy point.  Either set to "max" or numeric value.
+#' @param plot.nrei If set to TRUE will output suitable NREI points shapefile and raster
+#'
+#' @return Outputs several shapefiles 
+#' @export
+#'
+#' @examples
+#' check.fish.pts(visit.summary, zrank = "max", plot.nrei = TRUE)
+check.fish.pts = function(data, zrank = "max", plot.nrei = FALSE){
+  
+  # get visit id from visit directory
+  visit.id = as.numeric(unlist(str_split(data$visit.dir, "VISIT_"))[2])
+  
+  # if shapefile doesn't exist but there's csv point data then create and output points shapefile:
+  
+  # - for nrei predicted fish locations
+  if(data$nrei.locs.shp == 'No' & data$nrei.locs.csv == 'Yes'){
+    create.pts(data$visit.dir, data$visit.crs, "predFishLocations.csv", "predFishLocations")
+  }
+
+  # - for chinook predicted redd locations
+  if(data$ch.redd.locs.shp == 'No' & data$ch.redd.locs.csv == 'Yes'){
+    create.pts(data$visit.dir, data$visit.crs, "chkPredReddLocs.csv", "chkPredReddLocs")
+  }
+  
+  # - for steelhead predicted redd locations
+  if(data$st.redd.locs.shp == 'No' & data$st.redd.locs.csv == 'Yes'){
+    create.pts(data$visit.dir, data$visit.crs, "sthdPredReddLocs.csv", "sthdPredReddLocs")
+  }  
+  
+  # - for nrei all points
+  if(plot.nrei == TRUE & data$all.nrei.pts.csv == 'No'){
+    print('Cannot create points because all NREI points csv does not exist for visit ' + str(visit.id))
+  }
+  
+  if(plot.nrei == TRUE & data$all.nrei.pts.csv == 'Yes'){
+    create.pts(data$visit.dir, data$visit.crs, "allNreiPts.csv", "allNreiPts")
+  }
+  
+  # create nrei suitable points shp and nrei raster
+  if(plot.nrei == TRUE & data$all.nrei.pts.csv == 'Yes'){
+    
+    # get path to csv file
+    pts.csv = unlist(list.files(path = visit.dir, pattern = "allNreiPts.csv", full.names = TRUE, recursive = TRUE, include.dirs = FALSE))
+    
+    # read in csv and subset by pval and Jph columns
+    pts.tb = read_csv(pts.csv) %>%
+      dplyr::filter(rad.step.gte.user.pval > 0 & nrei_Jph > 0)
+    
+    # for each xy nrei point, there are multiple values throughout the water column (z)
+    # this workflow selects a single z value based on user defined argument 'zmax'
+    if(zrank == "max"){
+      pts.zrank = pts.tb %>%
+        group_by(X, Y) %>%
+        mutate(max.nrei = max(nrei_Jph),
+          is.max.nrei = near(max.nrei, nrei_Jph)) %>% # ask NK why this line is necessary
+        filter(is.max.nrei) %>%
+        mutate(nrei_js = max.nrei) %>%
+        select(idx, X, Y, Z, zrank, rad.step.gte.user.pval, nrei_Jph)
+    }else if(is.numeric(zrank) == TRUE){
+      my.zrank = zrank
+      pts.zrank = pts.tb %>%
+        dplyr::filter(zrank == my.zrank)
+    }else{
+      "Warning: zrank parameter must either be set to 'max' or a numeric value"
+    }
+    
+    # get count of number of rows in zrank points
+    # ...may be instances where reach has no 'suitable' nrei values
+    n.rows = dim(pts.zrank)[1]
+    
+    # create nrei suitable points shapefile and raster it don't already exist
+    if(data$suit.nrei.pts.shp == 'No' & n.rows > 0){
+      coords = cbind(pts.zrank$X, pts.zrank$Y)
+      zrank.spdf = SpatialPointsDataFrame(coords, pts.zrank, proj4string = CRS(data$visit.crs))
+      writeOGR(zrank.spdf, dirname(pts.csv), "suitableNreiPts", driver="ESRI Shapefile", overwrite_layer = TRUE)
+      zrank.pixels = SpatialPixelsDataFrame(zrank.spdf, tolerance=.001, zrank.spdf@data)
+      zrank.raster = raster(zrank.pixels[,'nrei_Jph'])
+      raster::writeRaster(zrank.raster, file.path(dirname(pts.csv), "suitableNreiRaster"), format="GTiff", overwrite = TRUE)
+    }
+  }
+}
+
+check.fish.pts(data, zrank, plot.nrei)
